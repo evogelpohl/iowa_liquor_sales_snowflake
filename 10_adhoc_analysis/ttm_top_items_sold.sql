44e7020d8a15f9db5e0f2d9a2603b618b0c5fac0
evogelpohl <evogelpohl@presidio.com>
2026-01-04 07:33:53 +0000

    edts

--- 10_adhoc_analysis/ttm_top_items_sold.sql
+++ 10_adhoc_analysis/ttm_top_items_sold.sql
@@ -3,17 +3,28 @@ USE SCHEMA IOWA_LIQUOR_SALES;
 USE WAREHOUSE IOWA_WH;
 
 WITH max_month AS (
-    SELECT
-        DATE_TRUNC('MONTH', MAX(SALE_DATE)) AS max_sale_month
-    FROM IOWA_LIQUOR_SALES
+  SELECT
+      DATE_TRUNC('MONTH', MAX(SALE_DATE)) AS max_sale_month
+  FROM IOWA_LIQUOR_SALES
+),
+
+ttm_sales AS (
+  SELECT
+      INITCAP(item_description) AS liquor_category,
+      SUM(SALE_DOLLARS)        AS ttm_sales
+  FROM IOWA_LIQUOR_SALES
+  CROSS JOIN max_month
+  WHERE SALE_DATE >= DATEADD(
+          MONTH, -11, max_month.max_sale_month
+        )
+    AND SALE_DATE < DATEADD(
+          MONTH,  1, max_month.max_sale_month
+        )
+  GROUP BY 1
 )
 
 SELECT
-    TO_VARCHAR(
-        SUM(SALE_DOLLARS),
-        '$999,999,999,999'
-    ) AS TTM_SALES
-FROM IOWA_LIQUOR_SALES
-CROSS JOIN max_month
-WHERE SALE_DATE >= DATEADD(MONTH, -11, max_month.max_sale_month)
-  AND SALE_DATE <  DATEADD(MONTH,  1, max_month.max_sale_month);
+    liquor_category        AS "Liquor Category",
+    round(ttm_sales,0)              AS "TTM Sales"
+FROM ttm_sales
+ORDER BY "TTM Sales" DESC;
